d197d09de24c99a2ba5e2212eeef2385efdbe45a
Алексей <alexey.tvalavadze@sbermegamarket.ru>
2023-05-15 12:30:27 +0300

    add list implementation and new tests

--- hw04_lru_cache/list.go
+++ hw04_lru_cache/list.go
@@ -17,10 +17,88 @@ type ListItem struct {
 }
 
 type list struct {
-	List // Remove me after realization.
-	// Place your code here.
+	head *ListItem
+	tail *ListItem
+	size int
 }
 
 func NewList() List {
 	return new(list)
+}
+
+func (l *list) Len() int {
+	return l.size
+}
+
+func (l *list) Front() *ListItem {
+	return l.head
+}
+
+func (l *list) Back() *ListItem {
+	return l.tail
+}
+
+func (l *list) PushFront(v interface{}) *ListItem {
+	newNode := &ListItem{Value: v, Next: l.head, Prev: nil}
+	if l.head != nil {
+		l.head.Prev = newNode
+	} else {
+		l.tail = newNode
+	}
+	l.head = newNode
+	l.size++
+	return newNode
+}
+
+func (l *list) PushBack(v interface{}) *ListItem {
+	newNode := &ListItem{Value: v, Next: nil, Prev: l.tail}
+	if l.tail != nil {
+		l.tail.Next = newNode
+	} else {
+		l.head = newNode
+	}
+	l.tail = newNode
+	l.size++
+	return newNode
+}
+
+func (l *list) Remove(i *ListItem) {
+	if i.Prev != nil {
+		i.Prev.Next = i.Next
+	} else {
+		l.head = i.Next
+	}
+	if i.Next != nil {
+		i.Next.Prev = i.Prev
+	} else {
+		l.tail = i.Prev
+	}
+	l.size--
+}
+
+func (l *list) MoveToFront(i *ListItem) {
+	if i == l.head {
+		return
+	}
+	// Remove the node from its current position
+	if i.Prev != nil {
+		i.Prev.Next = i.Next
+	} else {
+		l.head = i.Next
+	}
+	if i.Next != nil {
+		i.Next.Prev = i.Prev
+	} else {
+		l.tail = i.Prev
+	}
+
+	// Move the node to the front
+	i.Prev = nil
+	i.Next = l.head
+	if l.head != nil {
+		l.head.Prev = i
+	} else {
+		l.tail = i
+	}
+	l.head = i
 }
--- hw04_lru_cache/list_test.go
+++ hw04_lru_cache/list_test.go
@@ -20,12 +20,14 @@ func TestList(t *testing.T) {
 
 		l.PushFront(10) // [10]
 		l.PushBack(20)  // [10, 20]
-		l.PushBack(30)  // [10, 20, 30]
-		require.Equal(t, 3, l.Len())
+		l.PushBack(20)  // [10, 20, 20]
+		l.PushBack(30)  // [10, 20, 20, 30]
+		l.PushFront(10) // [10, 10, 20, 20, 30]
+		require.Equal(t, 5, l.Len())
 
-		middle := l.Front().Next // 20
-		l.Remove(middle)         // [10, 30]
-		require.Equal(t, 2, l.Len())
+		middle := l.Front().Next.Next // 20
+		l.Remove(middle)              // [10, 10, 20, 30]
+		require.Equal(t, 4, l.Len())
 
 		for i, v := range [...]int{40, 50, 60, 70, 80} {
 			if i%2 == 0 {
@@ -33,19 +35,19 @@ func TestList(t *testing.T) {
 			} else {
 				l.PushBack(v)
 			}
-		} // [80, 60, 40, 10, 30, 50, 70]
+		} // [80, 60, 40, 10, 10, 20, 30, 50, 70]
 
-		require.Equal(t, 7, l.Len())
+		require.Equal(t, 9, l.Len())
 		require.Equal(t, 80, l.Front().Value)
 		require.Equal(t, 70, l.Back().Value)
 
-		l.MoveToFront(l.Front()) // [80, 60, 40, 10, 30, 50, 70]
-		l.MoveToFront(l.Back())  // [70, 80, 60, 40, 10, 30, 50]
+		l.MoveToFront(l.Front()) // [80, 60, 40, 10, 10, 20, 30, 50, 70]
+		l.MoveToFront(l.Back())  // [70, 80, 60, 40, 10, 10, 20, 30, 50]
 
 		elems := make([]int, 0, l.Len())
 		for i := l.Front(); i != nil; i = i.Next {
 			elems = append(elems, i.Value.(int))
 		}
-		require.Equal(t, []int{70, 80, 60, 40, 10, 30, 50}, elems)
+		require.Equal(t, []int{70, 80, 60, 40, 10, 10, 20, 30, 50}, elems)
 	})
 }

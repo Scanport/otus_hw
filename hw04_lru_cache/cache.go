1e9f15cb9f0f0760042ac99f58fcdc12fd2d32d6
Алексей <alexey.tvalavadze@sbermegamarket.ru>
2023-05-16 18:18:18 +0300

    delete findKeysByValue

--- hw04_lru_cache/cache.go
+++ hw04_lru_cache/cache.go
@@ -2,6 +2,11 @@ package hw04lrucache
 
 type Key string
 
+type data struct {
+	k Key
+	v interface{}
+}
+
 type Cache interface {
 	Set(key Key, value interface{}) bool
 	Get(key Key) (interface{}, bool)
@@ -25,37 +30,27 @@ func NewCache(capacity int) Cache {
 func (c *lruCache) Set(key Key, value interface{}) bool {
 	if item, exists := c.items[key]; exists {
 		// Update existing item
-		item.Value = value
+		item.Value = data{key, value}
 		c.queue.MoveToFront(item)
 		return true
 	}
 	// Add new item
-	item := c.queue.PushFront(value)
+	item := c.queue.PushFront(data{key, value})
 	c.items[key] = item
 
 	// Remove the least recently used item if cache is full
 	if c.queue.Len() > c.capacity {
 		oldest := c.queue.Back()
-		delKey := findKeysByValue(c.items, oldest)
-		delete(c.items, delKey)
+		delete(c.items, oldest.Value.(data).k)
 		c.queue.Remove(oldest)
 	}
 	return false
 }
 
-func findKeysByValue(m map[Key]*ListItem, val *ListItem) Key {
-	for key, item := range m {
-		if item == val {
-			return key
-		}
-	}
-	return ""
-}
-
 func (c *lruCache) Get(key Key) (interface{}, bool) {
 	if item, exists := c.items[key]; exists {
 		c.queue.MoveToFront(item)
-		return item.Value, true
+		return item.Value.(data).v, true
 	}
 	return nil, false
 }
